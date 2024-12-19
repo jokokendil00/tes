2324a5171b3433b9543b80f6e467cabd51611196
jokokendil00 <149130145+jokokendil00@users.noreply.github.com>
2024-12-19 02:51:56 +0000

    hai

--- api/index.ts
+++ api/index.ts
@@ -1,8 +1,9 @@
 const express = require("express");
 const app = express();
+const path = require('path')
 
 app.get("/", (req, res) => {
-    res.send("Hello Express app!");
+    res.sendFile(path.join(__dirname, '..', 'components', 'home.htm'))
 });
 
 app.get("/auth", (req, res) => {
