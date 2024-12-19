eb6dfe5cbbdc43654907b550ab23ff223f1b3539
jokokendil00 <149130145+jokokendil00@users.noreply.github.com>
2024-12-19 02:49:24 +0000

    bang

--- api/index.ts
+++ api/index.ts
@@ -1,12 +1,14 @@
-
-
-const express = require('express');
+const express = require("express");
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello Express app!')
-})
+app.get("/", (req, res) => {
+    res.send("Hello Express app!");
+});
 
-app.listen(3000, () => console.log('Server ready on port 3000.'));
+app.get("/auth", (req, res) => {
+    res.send("auth bang");
+});
+
+app.listen(3000, () => console.log("Server ready on port 3000."));
 
 module.exports = app;
